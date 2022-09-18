7fd088ed76ca26e2c01f064831a4f2de800ce962
Glude <49980627+Glude@users.noreply.github.com>
2022-09-18 10:51:35 +0200

    ...

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,8 @@ fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
 
 author 'Glude'
-description 'StreetV Hud by Glude'
+description 'Hud by Glude'
 version '1.0.0'
-shared_script '@es_extended/imports.lua'
 
 client_scripts {
     'client.lua',
